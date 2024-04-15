2dc5f4433704d1b2709139ef81029c58eac59e27
clairepettit <25cpettit@castilleja.org>
2024-04-15 09:11:37 -0700

    Initial Commit

---
+++ Study_Buddy/Study_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_BuddyApp.swift
+//  Study_Buddy
+//
+//  Created by Claire Pettit on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Study_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
